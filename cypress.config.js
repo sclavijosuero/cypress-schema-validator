608f93f5e0ab649611f87816ca945803a37f00eb
Jan Joska <jn.joska@gmail.com>
2026-03-19 18:03:40 +0100

    chore: add expose config and set allowCypressEnv: false for Cypress 15

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  expose: {
+    disableSchemaValidation: false,
+    enableMismatchesOnUI: true,
+  },
+
   viewportWidth: 1920,
   viewportHeight: 1080,
-  
+
   watchForFileChanges: false,
-  
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
